d57b11cbdf7e5aeb30c24af67bff75145b0c9c2e
Oleksii Mazurenko <rebornemptymail@gmail.com>
2026-02-20 17:48:41 +0200

    Add post function for feedback, add fetchGenres

--- src/js/api/feedbackService.js
+++ src/js/api/feedbackService.js
@@ -3,3 +3,7 @@ import { http } from './http';
 export function fetchFeedbacks() {
   return http.get('/feedbacks', { params: { limit: 10, page: 1 } });
 }
+
+export function postFeedback({ name, rating, descr }) {
+  return http.post('/feedbacks', { name, rating, descr });
+}
