0a9f5e3c60274a053d6ea934d4964b4f6a0956c0
Polina Matviienko <polinamatviienko030@gmail.com>
2026-02-23 21:04:42 +0200

    add filters layout

---
+++ src/js/filters.js
@@ -0,0 +1,16 @@
+export const initMobileFilters = () => {
+  const filterBtn = document.querySelector('[data-filter-btn]');
+
+  if (!filterBtn) return;
+
+  filterBtn.addEventListener('click', () => {
+    const filterWrapper = filterBtn.closest('[data-filter-wrapper]');
+
+    if (filterWrapper) {
+      filterWrapper.classList.toggle('is-open');
+
+      const isOpen = filterWrapper.classList.contains('is-open');
+      filterBtn.setAttribute('aria-expanded', isOpen);
+    }
+  });
+};
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import { initMobileMenu } from './mobile-menu.js';
+import { initMobileFilters } from './filters.js';
 
 initMobileMenu();
+initMobileFilters();
 
 // Artists section
 import './artists.js';
