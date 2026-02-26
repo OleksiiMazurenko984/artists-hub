66c8c0435d204b0b903be0652a384f5f3e2d0930
Oleh <roadto12mmr@gmail.com>
2026-02-26 14:43:16 +0100

    add sorting desc and asc

--- src/js/api/fetchArtist.js
+++ src/js/api/fetchArtist.js
@@ -1,7 +1,13 @@
 import { http } from './http';
 
-export function fetchArtists({ page = 1, limit = 8 } = {}) {
-  return http.get('/artists', { params: { page, limit } });
+export function fetchArtists({ page = 1, limit = 8, sortName } = {}) {
+  const params = { page, limit };
+
+  if (sortName) {
+    params.sortName = sortName;
+  }
+
+  return http.get('/artists', { params });
 }
 export function fetchArtistById(id) {
   return http.get(`/artists/${id}`);
