d086aa4a765bbaf9e7435ec4a7a3bae9cc0f6b71
Yana <tschyana@gmail.com>
2026-02-26 14:01:58 -0500

    search by name implemented. name param added to sorting, load more

--- src/js/api/fetchArtist.js
+++ src/js/api/fetchArtist.js
@@ -1,10 +1,14 @@
 import { http } from './http';
 
-export function fetchArtists({ page = 1, limit = 8, sortName } = {}) {
+export function fetchArtists({ page = 1, limit = 8, name, sortName } = {}) {
   const params = { page, limit };
 
   if (sortName) {
     params.sortName = sortName;
+  }
+
+  if (name) {
+    params.name = name;
   }
 
   return http.get('/artists', { params });
