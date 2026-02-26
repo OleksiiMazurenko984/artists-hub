b4d3525d5c640c0bb6e7657e77da3fe69f34dfc5
Polina Matviienko <polinamatviienko030@gmail.com>
2026-02-26 12:01:35 +0200

    final correction

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,10 @@
 import { initMobileMenu } from './mobile-menu.js';
+import { toggleFilterMenu } from './filter-menu.js';
 import './components/slider';
 import './render/renderFeedbacks.js';
 
 initMobileMenu();
+toggleFilterMenu();
 
 import './artists.js';
 import './artist-modal.js';
