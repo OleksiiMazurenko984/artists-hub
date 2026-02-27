eebcc219ee7bc61d72b689c6e308b084c6ba9086
Oleh <roadto12mmr@gmail.com>
2026-02-27 12:18:07 +0100

    add filter by genres

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 import { initMobileMenu } from './mobile-menu.js';
 import { toggleFilterMenu } from './filter-menu.js';
+import { renderGenres } from './render/renderGenres.js';
 import './components/slider';
 import './render/renderFeedbacks.js';
 
 initMobileMenu();
 toggleFilterMenu();
+renderGenres();
 
 import './artists.js';
 import './artist-modal.js';
